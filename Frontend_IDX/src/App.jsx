a1c9d544ca803929f74c7a00ed6c3cace3bff5b4
Archisman <archismandas1405@gmail.com>
2024-12-15 18:50:08 +0530

    Added React Queries

--- Frontend_IDX/src/App.jsx
+++ Frontend_IDX/src/App.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react'
 import './App.css'
+import { pingApi } from './apis/ping';
 
 function App() {
+
+  useEffect(()=> {
+    pingApi();
+  },[]);
+
   return (
     <>HELLO </>
   )
