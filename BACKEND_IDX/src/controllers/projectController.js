ea09072ddde123e17f1b07d94c647f9c564870d7
Archisman <archismandas1405@gmail.com>
2024-12-17 00:21:43 +0530

    Added code creating a new project in the backend

--- BACKEND_IDX/src/controllers/projectController.js
+++ BACKEND_IDX/src/controllers/projectController.js
@@ -1,3 +1,28 @@
-export const createProjectController = async (req,res) => {
+import util from 'util';
+import child_process from 'child_process';
+import fs from 'fs/promises';
+import uuid4 from 'uuid4';
 
+/*
+
+    Create a unique id and then inside the projects folder create a new folder with that id
+
+*/
+
+const execPromisified = util.promisify(child_process.exec);
+
+export const createProjectController = async (req,res) => {
+    const projectId = uuid4();
+    console.log("New Project Id is :",projectId);
+
+    //Project folder created
+    await fs.mkdir(`./projects/${projectId}`);
+
+    //After this call the npm create vite@latest in the newly created folder
+
+    const response = await execPromisified('npm create vite@latest codeGround -- --template react',{
+        cwd:`./projects/${projectId}`
+    });
+
+    return res.json({ message: 'Project Created' , data : projectId});
 };
